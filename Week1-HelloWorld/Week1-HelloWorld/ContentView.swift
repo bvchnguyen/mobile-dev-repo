2f5782d4851e9fda906c226c15f96f18bde340c2
Bach Nguyen <bang4509@colorado.edu>
2023-09-09 22:25:30 -0600

    Separated circle into a component. Render it on toggle.

--- Week1-HelloWorld/Week1-HelloWorld/ContentView.swift
+++ Week1-HelloWorld/Week1-HelloWorld/ContentView.swift
@@ -7,39 +7,51 @@
 
 import SwiftUI
 
+struct CircleView: View{
+    var body: some View{
+        ZStack{
+            Circle()
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 300)
+                .overlay(
+                    Circle()
+                        .stroke(Color.blue, lineWidth: 1)
+                )
+            Text("hehehehehe")
+                .bold()
+                .font(.system(size: 24))
+                .foregroundColor(.white)
+        }
+    }
+}
+
 struct ContentView: View {
+    @State private var isToggle = false
     var body: some View {
         VStack {
-            ZStack{
-                Circle()
-                    .foregroundColor(.blue)
-                    .frame(width: 300, height: 300)
-                    .overlay(
-                        Circle()
-                            .stroke(Color.blue, lineWidth: 1)
-                    )
-                Text("My Name is World")
-                    .bold()
-                    .font(.system(size: 24))
-                    .foregroundColor(.white)
+            Toggle("Toggle me", isOn: $isToggle)
+                .padding()
+            if isToggle{
+                CircleView()
+            } else{
+                Text("Turn it ON!!!!")
             }
-
-            Button(action: {
-                print("World")
-            }){
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello World!")
-
-            }
-            .padding()
-            .foregroundColor(.blue)
-            .overlay(
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(Color.blue, lineWidth: 1)
-                    .frame(width: 200, height: 50)
-            )
+//            Button(action: {
+//                print("World")
+//            }){
+//                Image(systemName: "globe")
+//                    .imageScale(.large)
+//                    .foregroundColor(.accentColor)
+//                Text("Hello World!")
+//
+//            }
+//            .padding()
+//            .foregroundColor(.blue)
+//            .overlay(
+//                RoundedRectangle(cornerRadius: 5)
+//                    .stroke(Color.blue, lineWidth: 1)
+//                    .frame(width: 200, height: 50)
+//            )
         }
         .padding()
     }
